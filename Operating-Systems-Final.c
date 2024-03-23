97d1d1063cfbc6358149253901c3f5bab97f6a11
Tamarpick <Tamarpick18@gmail.com>
2024-03-23 20:22:33 +0200

    Part A question a

---
+++ Operating-Systems-Final.c
@@ -0,0 +1,23 @@
+//חלק א' סעיף א'
+
+function Wellcome() {
+    // נשתמש ב ECHO כדי להדפיס את ההודעות המפורמטות למסך
+    echo "aSPY//YASa"
+    echo "apyyyyCY//////////YCa |"
+    echo "sY//////YSpcs scpCY//Pp | Welcome to myShell"
+    echo "ayp ayyyyyyySCP//Pp syY//C | Version 2.4.3"
+    echo "AYAsAYYYYYYYY///Ps cY//S |"
+    echo "pCCCCY//p cSSps y//Y | https://github.com/Tamarpick"
+    echo "SPPPP///a pP///AC//Y |"
+    echo "A//A cyP////C | Have fun!"
+    echo "p///Ac sC///a |"
+    echo "P////YCpc A//A | Craft packets like it is your last"
+    echo "scccccp///pSP///p p//Y | day on earth."
+    echo "sY/////////y caa S//P | -- Lao-Tze"
+    echo "cayCyayP//Ya pY/Ya |"
+    echo "sY/PsY////YCc aC//Yp"
+    echo "sc sccaCY//PCypaapyCP//YSs"
+    echo "spCPY//////YPSps"
+    echo "ccaacs"
+}
+
